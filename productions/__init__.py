ece8b70dccc1173edc25e1c18e5439fab4105bf9
Michal Barczyk <barczykmt@gmail.com>
2022-12-15 19:33:09 +0100

    add base version of p5

--- productions/__init__.py
+++ productions/__init__.py
@@ -1,3 +1,4 @@
 from .p1 import P1
 from .p2 import P2
+from .p5 import P5
 from .p12 import P12, P12_prim
--- productions/decorators.py
+++ productions/decorators.py
@@ -11,3 +11,17 @@ def first_isomorphism(left_side):
 
         return inner
     return outer
+
+def isomorphism_P5(left_side):
+    def outer(apply_fun):
+        def inner(G, *args, **kwargs):
+            isomorphisms = find_isomorphisms(G, left_side)
+
+            print(str(isomorphisms))
+
+            if not isomorphisms: return apply_fun(G, *args, **kwargs)
+
+            return apply_fun(G, *args, isomorphism=isomorphisms[0], **kwargs)
+
+        return inner
+    return outer
