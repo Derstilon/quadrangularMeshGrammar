2cfcdfd1a377ea379cbf699bcf58c3bc57ac0b76
Łukasz Wroński <lukaszwronski98@gmail.com>
2022-12-15 21:16:45 +0100

    Add P6 production

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 import networkx as nx
 
-from productions import P1, P5
+from productions import P1, P5, P6
 from visualization import draw_graph
 
 
@@ -17,22 +17,43 @@ def main(args):
     # P1.apply(G)
     # P5.apply(G)
 
+    # G = nx.Graph()
+    # G.add_node(1, label='I', pos=(0.5, 0.5), layer=0)
+    # G.add_node(2, label='E', pos=(0, 0), layer=0)
+    # G.add_node(3, label='E', pos=(1, 0), layer=0)
+    # G.add_node(5, label='E', pos=(1, 1), layer=0)
+    # G.add_node(4, label='E', pos=(0, 1), layer=0)
+    # G.add_node(6, label='E', pos=(0, 0.5), layer=0)
+    # G.add_edges_from([(1, i) for i in range(2, 6)])
+    # G.add_edge(2, 3)
+    # G.add_edge(3, 5)
+    # G.add_edge(4, 5)
+    # G.add_edge(2, 6)
+    # G.add_edge(4, 6)
+
+    # #
+    # P5.apply(G)
+
+    # draw_graph(G)
+    
     G = nx.Graph()
     G.add_node(1, label='I', pos=(0.5, 0.5), layer=0)
     G.add_node(2, label='E', pos=(0, 0), layer=0)
     G.add_node(3, label='E', pos=(1, 0), layer=0)
     G.add_node(5, label='E', pos=(1, 1), layer=0)
     G.add_node(4, label='E', pos=(0, 1), layer=0)
+    G.add_node(6, label='E', pos=(0, 0.5), layer=0)
+    G.add_node(7, label='E', pos=(0.5, 0), layer=0)
     G.add_edges_from([(1, i) for i in range(2, 6)])
-    G.add_edge(2, 3)
     G.add_edge(3, 5)
     G.add_edge(4, 5)
     G.add_edge(2, 6)
     G.add_edge(4, 6)
+    G.add_edge(2, 7)
+    G.add_edge(3, 7)
 
-    G.add_node(6, label='E', pos=(0, 0.5), layer=0)
     #
-    P5.apply(G)
+    P6.apply(G)
 
     draw_graph(G)
 
--- productions/p6.py
+++ productions/p6.py
@@ -1,25 +1,27 @@
 import networkx as nx
-from productions.decorators import first_isomorphism, isomorphism_P5
+from productions.decorators import first_isomorphism, isomorphism_P5, isomorphism_P6
 from typing import Dict
 
 
-class P5():
+class P6():
     left = nx.Graph()
     left.add_node(1, label='I')
     left.add_node(2, label='E')
     left.add_node(3, label='E')
-    left.add_node(5, label='E')
     left.add_node(4, label='E')
+    left.add_node(5, label='E')
     left.add_node(6, label='E')
+    left.add_node(7, label='E')
     left.add_edges_from([(1, i) for i in range(2, 6)])
     left.add_edge(3, 5)
-    left.add_edge(2, 3)
+    left.add_edge(4, 5)
     left.add_edge(2, 6)
     left.add_edge(4, 6)
-    left.add_edge(4, 5)
+    left.add_edge(2, 7)
+    left.add_edge(3, 7)
 
     @staticmethod
-    @isomorphism_P5(left)
+    @isomorphism_P6(left)
     def apply(G: nx.Graph, isomorphism: Dict = None):
         if isomorphism is None:
             return False
@@ -40,7 +42,8 @@ class P5():
 
         layer = I_node['layer']
         pos = sorted(E_pos, key=lambda e: (e[1], e[0]))
-        [(x1, y1), (x2, y2), (x5, y5), (x3, y3), (x4, y4)] = pos
+        print(pos)
+        [(x1, y1), (x6, y6), (x2, y2), (x5, y5), (x3, y3), (x4, y4)] = pos
 
         size = G.number_of_nodes()
 
