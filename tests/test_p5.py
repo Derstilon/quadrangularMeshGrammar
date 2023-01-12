3351bc12435a6da292a9609c2c592cc3c676d271
Michal Barczyk <barczykmt@gmail.com>
2023-01-12 19:17:33 +0100

    add more tests of p5

--- tests/test_p5.py
+++ tests/test_p5.py
@@ -23,26 +23,6 @@ def test_p5_should_be_applied_right():
 
     assert True == P5.apply(G)
 
-    # todo
-    # assert [(1, {'label': 'el', 'pos': (0.5, 0.5), 'layer': 0}),
-    #         (2, {'label': 'i', 'pos': (0.5, 0.5), 'layer': 1}),
-    #         (3, {'label': 'E', 'pos': (0, 0), 'layer': 1}),
-    #         (4, {'label': 'E', 'pos': (1, 0), 'layer': 1}),
-    #         (5, {'label': 'E', 'pos': (0, 1), 'layer': 1}),
-    #         (6, {'label': 'E', 'pos': (1, 1), 'layer': 1}),
-    #         (7, {'label': 'I', 'pos': (0.25, 0.5), 'layer': 2}),
-    #         (8, {'label': 'I', 'pos': (0.75, 0.5), 'layer': 2}),
-    #         (9, {'label': 'E', 'pos': (0, 0), 'layer': 2}),
-    #         (10, {'label': 'E', 'pos': (0.5, 0.0), 'layer': 2}),
-    #         (11, {'label': 'E', 'pos': (1, 0), 'layer': 2}),
-    #         (12, {'label': 'E', 'pos': (0, 1), 'layer': 2}),
-    #         (13, {'label': 'E', 'pos': (0.5, 1.0), 'layer': 2}),
-    #         (14, {'label': 'E', 'pos': (1, 1), 'layer': 2})] == list(G.nodes(data=True))
-    #
-    # assert {(1, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 8), (2, 7), (3, 4), (3, 6), (4, 5),
-    #         (5, 6), (7, 9), (7, 10), (7, 12), (7, 13), (8, 10), (8, 11), (8, 13), (8, 14),
-    #         (9, 10), (9, 12), (10, 11), (10, 13), (11, 14), (12, 13), (13, 14)} == set(G.edges)
-
 
 def test_p5_should_be_applied_top():
     G = nx.Graph()
@@ -78,4 +58,40 @@ def test_p5_should_be_applied_left():
     G.add_edge(2, 3)
 
     assert True == P5.apply(G)
+
+
+def test_p5_should_be_not_applied_wrong_label():
+    G = nx.Graph()
+    G.add_node(1, label='I', pos=(0.5, 0.5), layer=0)
+    G.add_node(2, label='E', pos=(0, 0), layer=0)
+    G.add_node(3, label='E', pos=(1, 0), layer=0)
+    G.add_node(5, label='E', pos=(1, 1), layer=0)
+    G.add_node(4, label='E', pos=(0, 1), layer=0)
+    G.add_node(6, label='I', pos=(0.5, 0), layer=0)
+    G.add_edges_from([(1, i) for i in range(2, 6)])
+    G.add_edge(2, 4)
+    G.add_edge(3, 5)
+    G.add_edge(4, 5)
+    G.add_edge(2, 6)
+    G.add_edge(3, 6)
+
+    assert False == P5.apply(G)
+
+
+def test_p6_should_be_not_applied_wrong_coordinates():
+    G = nx.Graph()
+    G.add_node(1, label='I', pos=(0.5, 0.5), layer=0)
+    G.add_node(2, label='E', pos=(0, 0), layer=0)
+    G.add_node(3, label='E', pos=(1, 0), layer=0)
+    G.add_node(5, label='E', pos=(1, 1), layer=0)
+    G.add_node(4, label='E', pos=(0, 1), layer=0)
+    G.add_node(6, label='E', pos=(0.3, 0), layer=0)
+    G.add_edges_from([(1, i) for i in range(2, 6)])
+    G.add_edge(2, 4)
+    G.add_edge(3, 5)
+    G.add_edge(4, 5)
+    G.add_edge(2, 6)
+    G.add_edge(3, 6)
+
+    assert False == P5.apply(G)
 
