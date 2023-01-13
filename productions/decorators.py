b90ca279170ec949ed98dbebce8070141e54b56e
Jakub Niechaj <quban123@gmail.com>
2023-01-13 18:03:14 +0100

    Add P3 and P4 gramar (#9)
    Co-authored-by: ostatni5 <26521377+ostatni5@users.noreply.github.com>

--- productions/decorators.py
+++ productions/decorators.py
@@ -12,6 +12,19 @@ def first_isomorphism(left_side):
         return inner
     return outer
 
+def all_isomorphisms(left_side):
+    def outer(apply_fun):
+        def inner(G, *args, **kwargs):
+            isomorphisms = find_isomorphisms(G, left_side)
+
+            if not isomorphisms: return apply_fun(G, *args, **kwargs)
+
+            return apply_fun(G, *args, isomorphisms=isomorphisms, **kwargs)
+            
+        return inner
+    return outer
+
+
 def first_isomorphism_for_p13(left_side):
     def outer(apply_fun):
         def inner(G, *args, **kwargs):
